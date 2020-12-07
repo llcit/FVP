fa23a5bd69adc732f616c2098cc13389369b0c12
lmegviar <lmegviar@gmail.com>
2020-12-07 17:10:24 -0500

    add base path in reset link

--- inc/ses_settings.php
+++ inc/ses_settings.php
@@ -5,6 +5,6 @@
 	  'usernameSmtp' => 'AKIA5GGQGDXJV552RJSH',
 	  'passwordSmtp' => 'BARWyTT6oToqa0t9SI+M93uZ2LTLZS+TBFHsXHtxAx40',
 	  'host' => 'email-smtp.us-east-1.amazonaws.com',
-	  'password_reset_base_url' => 'http://3.91.222.187/FVP/'
+	  'password_reset_base_url' => 'http://3.91.222.187/FVP'
 	];
 ?>
