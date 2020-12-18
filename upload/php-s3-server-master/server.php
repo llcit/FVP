6887277ccb3bbf6b806a5bec7b262c4102f1e9de
lmegviar <lmegviar@gmail.com>
2020-12-18 09:55:11 -0500

    WIP: transcribe

--- upload/php-s3-server-master/server.php
+++ upload/php-s3-server-master/server.php
@@ -28,7 +28,7 @@
             $linkPromise = new Promise();
             $linkPromise->resolve(true);
             $audioPromise = new Promise();
-            $audioPromise->wait()you;
+            $audioPromise->wait();
             $audioPromise->resolve(true);
             $transcribePromise = new Promise();
             $transcribePromise->resolve(true);
