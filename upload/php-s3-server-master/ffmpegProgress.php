4f966eba59c12d967950da8e662bac8795cfb785
lmegviar <lmegviar@gmail.com>
2020-12-29 08:41:56 -0500

    enquote type

--- upload/php-s3-server-master/ffmpegProgress.php
+++ upload/php-s3-server-master/ffmpegProgress.php
@@ -1,6 +1,6 @@
 <?php
-  include "../inc/db_pdo.php";
-  include "../inc/sqlFunctions.php";
+  include "../../inc/db_pdo.php";
+  include "../../inc/sqlFunctions.php";
 	$pid = getPid($_GET['uid'],$_GET['eid'],$_GET['presentation_type']);
 	if (file_exists('./progress/'.$_GET['pid'].'.txt')) {
 		$progress = file_get_contents('./progress/'.$_GET['pid'].'.txt');
