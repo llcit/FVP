afd496ffefd035661b88a3b34d0cc69cfa6e1e0c
lmegviar <lmegviar@gmail.com>
2020-12-29 10:06:21 -0500

    fix pid on progress

--- upload/php-s3-server-master/ffmpegProgress.php
+++ upload/php-s3-server-master/ffmpegProgress.php
@@ -2,10 +2,10 @@
   include "../../inc/db_pdo.php";
   include "../../inc/sqlFunctions.php";
 	$pid = getPid($_GET['uid'],$_GET['eid'],$_GET['presentation_type']);
-	if (file_exists('./progress/'.$_GET['pid'].'.txt')) {
+	if (file_exists('./progress/'.$pid.'.txt')) {
 		$progress = file_get_contents('./progress/'.$_GET['pid'].'.txt');
 		if ($progress == 100) {
-			unlink('./progress/'.$_GET['pid'].'.txt');
+			unlink('./progress/'.$pid.'.txt');
 			echo ('100');
 		}
 		else {
