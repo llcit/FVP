c6e6554e4942e182cc7b710d21fa298cf0dbc76d
lmegviar <lmegviar@gmail.com>
2021-02-13 15:43:05 -0500

    abstract mailer

--- inc/SESMailer.php
+++ inc/SESMailer.php
@@ -6,27 +6,27 @@
 		$bodyText = $vars['bodyText'];
 		$bodyHtml = $vars['bodyHtml']; 
 	  try {
-	    $mail->CharSet =  "utf-8";
-	    $mail->isSMTP();
-	    $mail->setFrom($SETTINGS['sender'], $SETTINGS['senderName']);
-	    $mail->Username   = $SETTINGS['usernameSmtp'];
-	    $mail->Password   = $SETTINGS['passwordSmtp'];
-	    $mail->Host       = $SETTINGS['hostSmtp'];
-	    $mail->Port       = $SETTINGS['portSmtp'];
-	    $mail->SMTPAuth   = true;
-	    $mail->SMTPSecure = 'tls';
-	    $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
-	    $mail->addAddress($recipient);
-	    $mail->isHTML(true);
-	    $mail->Subject    = $subject;
-	    $mail->Body       = $bodyHtml;
-	    $mail->AltBody    = $bodyText;
-	    $mail->Send();
+	    $mailer->CharSet =  "utf-8";
+	    $mailer->isSMTP();
+	    $mailer->setFrom($SETTINGS['sender'], $SETTINGS['senderName']);
+	    $mailer->Username   = $SETTINGS['usernameSmtp'];
+	    $mailer->Password   = $SETTINGS['passwordSmtp'];
+	    $mailer->Host       = $SETTINGS['hostSmtp'];
+	    $mailer->Port       = $SETTINGS['portSmtp'];
+	    $mailer->SMTPAuth   = true;
+	    $mailer->SMTPSecure = 'tls';
+	    $mailer->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+	    $mailer->addAddress($recipient);
+	    $mailer->isHTML(true);
+	    $mailer->Subject    = $subject;
+	    $mailer->Body       = $bodyHtml;
+	    $mailer->AltBody    = $bodyText;
+	    $mailer->Send();
 	    $userMsg = "success";
 	  } catch (phpmailerException $e) {
 	      $userMsg =  "An error occurred. {$e->errorMessage()}"; //Catch errors from PHPMailer.
 	  } catch (Exception $e) {
-	      $userMsg =  "Email not sent. {$mail->ErrorInfo}"; //Catch errors from Amazon SES.
+	      $userMsg =  "Email not sent. {$mailer->ErrorInfo}"; //Catch errors from Amazon SES.
 	  }
 	  return $userMsg;
 	}
