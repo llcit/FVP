8d36e3c0bb4afc211433bcfdc99d660d959c5eba
lmegviar <lmegviar@gmail.com>
2021-02-13 15:41:29 -0500

    abstract mailer

--- passwordSetup.php
+++ passwordSetup.php
@@ -3,7 +3,7 @@
     <head>
       <?php
         $SETTINGS = parse_ini_file(__DIR__."/inc/settings.ini");
-        require '/vendor/autoload.php';
+        require 'vendor/autoload.php';
         use PHPMailer\PHPMailer\PHPMailer;
         use PHPMailer\PHPMailer\Exception;
         include "./inc/dump.php";
