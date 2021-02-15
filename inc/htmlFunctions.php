b69e439bdaa939d7dcb8565ded82220085259de2
lmegviar <lmegviar@gmail.com>
2021-02-15 11:18:47 -0500

    clean up links on front page

--- inc/htmlFunctions.php
+++ inc/htmlFunctions.php
@@ -1,35 +1,37 @@
 <?php
-	function writePageHeader($base_url,$user,$pageTitle) {
+	function writePageHeader($base_url,$user=null,$pageTitle) {
 		$navLinks = writeNavLinks($user->role,'header');
 		$pageTitle = "Flagship Video Project";
-		$userName = "<h5 style='display:inline'>" . $user->first_name . " " . $user->last_name . "</h5>";
-		$welcomeMsg = "
-          $userName 
-          <a href='".$base_url."/logout.php' class='btn btn-xs btn-icon btn-danger'>
-            <i class='fa fa-sign-out-alt' aria-hidden='true'></i>
-          </a>
-        ";
-		  $cdnDependencies = writeCDNDepandencies();
-      $html = "
-	      <div class='panel-heading fv_heading' style='overflow:none;'>
-	        <div class='row flex-nowrap'>
-	          <div class='col-3'>
-	            <img src='".$base_url."/img/logo_lf.png' class='logo-img-fluid'>
-	          </div>
-	          <div class='pageTitle col-6'>
-	          		$pageTitle
-	          </div>
-	          <div class='col-3'>
-	            <img src='".$base_url."/img/logo_ac.png' class='logo-img-fluid float-right'>
-	          </div>
-	        </div>
-	      </div>
-	      <div class='fv_subHeader'>
-	        $navLinks
-	        $welcomeMsg
-	      </div>
-      ";
-      return $cdnDependencies . $html;
+		if ($user->first_name != '' ) {
+			$userName = "<h5 style='display:inline'>" . $user->first_name . " " . $user->last_name . "</h5>";
+			$welcomeMsg = "
+	          $userName 
+	          <a href='".$base_url."/logout.php' class='btn btn-xs btn-icon btn-danger'>
+	            <i class='fa fa-sign-out-alt' aria-hidden='true'></i>
+	          </a>
+	        ";
+    }
+	  $cdnDependencies = writeCDNDepandencies();
+    $html = "
+      <div class='panel-heading fv_heading' style='overflow:none;'>
+        <div class='row flex-nowrap'>
+          <div class='col-3'>
+            <img src='".$base_url."/img/logo_lf.png' class='logo-img-fluid'>
+          </div>
+          <div class='pageTitle col-6'>
+          		$pageTitle
+          </div>
+          <div class='col-3'>
+            <img src='".$base_url."/img/logo_ac.png' class='logo-img-fluid float-right'>
+          </div>
+        </div>
+      </div>
+      <div class='fv_subHeader'>
+        $navLinks
+        $welcomeMsg
+      </div>
+    ";
+    return $cdnDependencies . $html;
 	}
 	function writeCDNDepandencies() {
 		$dependencies = "
--- index.php
+++ index.php
@@ -17,54 +17,32 @@
 			  else {
 			  	$user = getUser($_SESSION['username']);
 			  	$role =  $user->role;
-			  	$userName = "<h5 style='display:inline'>" . $user->first_name . " " . $user->last_name . "</h5>";
-          if ($user) {
-            $welcomeMsg = "
-              Welcome $userName! 
-              <a href='".$SETTINGS['base_url']."/logout.php' class='btn btn-xs btn-icon btn-danger'>
-                <i class='fa fa-sign-out-alt' aria-hidden='true'></i>
-              </a>
-            ";
-          }
 			  }
         $pageContent = writeNavLinks($role,'body');
       ?>
-      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
       <link rel="stylesheet" href="./css/main.css" type="text/css"/>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-      <script>
-
-      </script>
     </head>
     <body>
       <div class="panel panel-default">
-        <div class="panel-heading fv_heading">
-          <img src='./img/logo_lf.png'>
-          <span class='pageTitle'>
-          		<?php echo($pageTitle); ?>
-          </span>
-          <span class='pull-right'>
-            <img src='./img/logo_ac.png'>
-          </span>
-        </div>
-        <div class='fv_subHeader'>
-          <?php echo($welcomeMsg); ?>
-        </div>
-        <form method="post" action="">
-          <div class="container">
-             <div class="row fv_main">
-                <div class="card fv_card">
-                    <div class="card-body fv_card_body" style='border-bottom:solid 1px gray;'>
-                       <h2 class="card-title"><?php echo($subTitle); ?></h2>
-                       <p class="card-text"><?php echo($titleText); ?></p>
-                    </div>
-                    <div class='fv_pageContent'>
-                      <?php echo($pageContent); ?>
-                    </div>
+        <?php 
+          $header = writePageHeader($SETTINGS['base_url'],$user,$pageTitle);
+          echo($header); 
+        ?>
+        <div class="panel-body" style='margin-top:30px;'>
+          <div class="container" >
+           <div class="row fv_main">
+              <div class="card fv_card">
+                <div class="card-body fv_card_body" style='border-bottom:solid 1px gray;'>
+                   <h2 class="card-title"><?php echo($subTitle); ?></h2>
+                   <p class="card-text"><?php echo($titleText); ?></p>
+                </div>
+                <div class='fv_pageContent'>
+                  <?php echo($pageContent); ?>
                 </div>
               </div>
+            </div>
           </div>
-        </form>
+        </div>
         <div class="footer">
           <p> </p>
         </div>
