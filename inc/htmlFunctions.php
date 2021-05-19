6b0bddd32a93b8bd103e84bc675551fc4d610f59
Aitor Arronte <aitoraalvarez@gmail.com>
2021-05-18 16:23:18 -1000

    add role staff to manage page

--- inc/htmlFunctions.php
+++ inc/htmlFunctions.php
@@ -63,12 +63,12 @@
       ['label'=>'Login','href'=>$SETTINGS['base_url'].'/login.php','req'=>['anonymous']],
       ['label'=> 'Your Videos','href'=>$SETTINGS['base_url'].'/personal.php','req'=>['student']],
       ['label'=>'Upload Video','href'=>$SETTINGS['base_url'].'/upload/','req'=>['student']],
-      ['label'=>'Manage','href'=>$SETTINGS['base_url'].'/manage/','req'=>['admin']],
+      ['label'=>'Manage','href'=>$SETTINGS['base_url'].'/manage/','req'=>['admin', 'staff']],
       ['label'=>'Video Showcase','href'=>$SETTINGS['base_url'].'/player/index.php?sc=1','req'=>[]],
       ['label'=>'Video Archive','href'=>$SETTINGS['base_url'].'/archive.php','req'=>['staff','admin']],
       ['label'=>'About This Site','href'=>$SETTINGS['base_url'].'/about.php','req'=>[]]
     ];
-    if ($context == 'header' || $role == 'staff') {
+    if ($context == 'header' ) {
       $class = 'linkList_header';
     }
     else {
